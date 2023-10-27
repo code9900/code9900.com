3bd2be037453c05d73376957bd8bd88518d0415e
code9900 <145799651+code999000@users.noreply.github.com>
2023-10-27 22:54:17 +0300

    Added my profile image

--- app/page.js
+++ app/page.js
@@ -1,14 +1,21 @@
 "use client";
 
 
+import Link from "next/link";
+import Image from "next/image";
+
 import {motion} from "framer-motion";
 
 
 export default function Home() {
   return (
-    <main className='layout min-h-screen w-screen'>
-      <motion.div initial={{opacity:0, y: -60, scale:0.6}} animate={{opacity:1, y: 0, scale: 1}} transition={{delay: 0.6}} className="bg-yellow-200 rounded-full layout h-6 w-1/2">
-        <h1 className="text-black scale-90 rotate-3 md:scale-100 md:rotate-0 hover:scale-90 hover:rotate-3 transition-all">Hi, I'm code9900</h1>
+    <main className='layout flex-col min-h-screen w-screen gap-12'>
+      <motion.img initial={{scale: 0.9, opacity: 0}} animate={{scale: 1, opacity: 1}} transition={{delay: 0.6}} src={"./pan.color.circle.svg"} height={250} width={250} alt="code9900 profile image"/>
+
+      <motion.div initial={{opacity:0, y: 60}} animate={{opacity:1, y: 0}} transition={{delay: 1.2}} className="layout flex-col w-full p-0">
+        <div className="bg-yellow-200 rounded-full layout h-6 w-1/2">
+          <h1 className="text-black scale-90 rotate-3 md:scale-100">Hi, I'm code9900</h1>
+        </div>
       </motion.div>
     </main>
   )
